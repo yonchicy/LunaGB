2949fcf69ca87763b5a6e127335652595b953a69
yonchicy <1079279184@qq.com>
2024-06-10 21:22:22 +0800

    直接抄完lab5

--- Programs/LunaGB/Emulator.hpp
+++ Programs/LunaGB/Emulator.hpp
@@ -1,13 +1,17 @@
 #pragma once
-
-#include "CPU.hpp"
-
 #include <Luna/Runtime/Result.hpp>
+#include "CPU.hpp"
 using namespace Luna;
+
+constexpr u8 INT_VBLANK = 1;
+constexpr u8 INT_LCD_STAT = 2;
+constexpr u8 INT_TIMER = 4;
+constexpr u8 INT_SERIAL = 8;
+constexpr u8 INT_JOYPAD = 16;
 
 struct Emulator
 {
-    byte_t * rom_data = nullptr;
+    byte_t* rom_data = nullptr;
     usize rom_data_size = 0;
 
     //! `true` if the emulation is paused.
@@ -17,12 +21,16 @@ struct Emulator
     //! The clock speed scale value.
     f32 clock_speed_scale = 1.0;
 
-
     CPU cpu;
+
     byte_t vram[8_kb];
     byte_t wram[8_kb];
     byte_t hram[128];
 
+    //! 0xFF0F - The interruption flags.
+    u8 int_flags;
+    //! 0xFFFF - The interruption enabling flags.
+    u8 int_enable_flags;
 
     RV init(const void* cartridge_data, usize cartridge_data_size);
     void update(f64 delta_time);
@@ -30,14 +38,12 @@ struct Emulator
     //! This is called from CPU instructions.
     //! @param[in] mcycles The number of machine cycles to tick.
     void tick(u32 mcycles);
-
-    u8 bus_read(u16 addr);
-    void  bus_write(u16 addr,u8 data);
-
-
     void close();
     ~Emulator()
     {
         close();
     }
+
+    u8 bus_read(u16 addr);
+    void bus_write(u16 addr, u8 data);
 };
